4cb1b2b5462022052bbfb99c984e4c995a5be4d0
Alody <tmahour@hotmail.com>
2024-12-17 18:30:59 +0000

    Made player a triangle (secretly a circle)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 import os
 os.environ['SDL_AUDIODRIVER'] = 'dsp'
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
@@ -12,11 +14,13 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(x=(SCREEN_WIDTH/2), y=(SCREEN_HEIGHT/2))
     while True:
         for event in pygame.event.get(): # loop until user quits
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black") # fill with black
+        player.draw(screen)
         pygame.display.flip() # update display
         dt = clock.tick(60) / 1000 # framerate to 60 fps and return amount of time since last call (delta time)
 
